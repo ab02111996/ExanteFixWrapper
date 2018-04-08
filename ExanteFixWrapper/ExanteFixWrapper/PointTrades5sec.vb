54fd298598096b175aeef7312159f10003849c47
ab02111996 <ab02111996@yandex.ru>
2018-04-08 11:08:48 +0700

    PointTrades5sec

---
+++ ExanteFixWrapper/ExanteFixWrapper/PointTrades5sec.vb
@@ -0,0 +1,19 @@
+﻿Public Class PointTrades5sec
+    Public openPrice As Double
+    Public closePrice As Double
+    Public highPrice As Double
+    Public lowPrice As Double
+    Public volumeSell As Double
+    Public volumeBuy As Double
+    Public time As DateTime
+
+    Public Sub New(buffer As Buffer)
+        openPrice = buffer.openPrice
+        closePrice = buffer.closePrice
+        highPrice = buffer.highPrice
+        lowPrice = buffer.lowPrice
+        volumeBuy = buffer.volumeBuy
+        volumeSell = buffer.volumeSell
+        time = buffer.endTimeFrame
+    End Sub
+End Class
