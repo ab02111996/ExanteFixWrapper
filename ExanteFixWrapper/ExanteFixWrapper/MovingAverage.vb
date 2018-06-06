7cf9e3d49a503c3f9bed2e9177208eef3cc823f4
Nord <Nord1993@gmail.com>
2018-06-06 22:01:57 +0700

    MovingAverage fix

--- ExanteFixWrapper/ExanteFixWrapper/MovingAverage.vb
+++ ExanteFixWrapper/ExanteFixWrapper/MovingAverage.vb
@@ -20,15 +20,18 @@
         Else
             'Далее все предыдущие значения сдвигаются в конец, а в начало списка добавляется текущее значение
             If windowPoints.Count >= windowSize Then
+                Dim windowPointsCopy = New List(Of Double)
+                windowPointsCopy.AddRange(windowPoints)
                 For i = 0 To windowSize - 1
                     If i < (windowSize - 1) Then
-                        windowPoints(i) = windowPoints(i + 1)
-                        sum += windowPoints(i)
+                        windowPointsCopy(i) = windowPointsCopy(i + 1)
+                        sum += windowPointsCopy(i)
                     Else
-                        windowPoints(i) = currentValue
-                        sum += windowPoints(i)
+                        windowPointsCopy(i) = currentValue
+                        sum += windowPointsCopy(i)
                     End If
                 Next
+                windowPoints = windowPointsCopy
             Else
                 windowPoints.Add(currentValue)
                 sum += currentValue
